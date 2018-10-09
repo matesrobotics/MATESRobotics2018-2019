37d28e0f624c7705f72a6f67e48d28d8ea04b4a9
Zach Cartnick <zachcartnick@gmail.com>
2018-10-09 15:14:58 -0400

    Reduced
    Cut out old unusable code

--- samples/BasicOpMode_Linear.java
+++ samples/BasicOpMode_Linear.java
@@ -81,13 +81,9 @@ public class BasicOpMode_Linear extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
