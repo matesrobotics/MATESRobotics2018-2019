3bd75dbec2e8ed96fde8ef2f9c8358d5e07a77e5
Zach Cartnick <zachcartnick@gmail.com>
2018-12-11 15:22:25 -0500

    Mirgrated Code to Android Studio

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.java
@@ -0,0 +1,55 @@
+// Copyright 2018-2019 FIRST Tech Challenge Team 12365
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class RobotMap {
+    /* Public OpMode members. */
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+    public DcMotor lift = null;
+    public DcMotor cBelt = null;
+    public Servo bridge = null;
+
+
+    /* local OpMode members. */
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public RobotMap(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap hwMap) {
+        // Save reference to Hardware map
+
+        // Define and Initialize Motors
+        leftDrive = hwMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hwMap.get(DcMotor.class, "rightDrive");
+        lift = hwMap.get(DcMotor.class, "lift");
+        cBelt = hwMap.get(DcMotor.class, "cBelt");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        lift.setPower(0);
+
+        // Set all motors to run with or without encoders.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        cBelt.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Define and initialize ALL installed servos.
+        bridge = hwMap.get(Servo.class, "bridge");
+        bridge.setPosition(1);
+    }
+}
+
