d886f4764566ea3370af5c7e6addbb9bdcde6f0e
Zach Cartnick <zachcartnick@gmail.com>
2019-01-10 18:46:45 -0500

    Progress... (1/10/19)
    still working on autonomous...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,4 +1,3 @@
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -11,53 +10,63 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
 
-public class Autonomous extends LinearOpMode {
+public class Auto extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     RobotMap robot = new RobotMap();
 
     //@Override //Code to run ONCE when the driver hits INIT
-    public void init() {
-        robot.init(hardwareMap);
-    }
+    //public void init() {
+    //  robot.init(hardwareMap);
+
+    //}
 
     @Override
     public void runOpMode() {
+
+        waitForStart();
+
+        robot.init(hardwareMap);
         runtime.reset();
-
         robot.lift.setPower(1);
-
-        while(runtime.seconds()<5) {
-            telemetry.
+        while(opModeIsActive() && runtime.seconds()<15) {
+            telemetry.addData("Lowering!", runtime);
+            telemetry.update();
         }
         robot.lift.setPower(0);
 
         //drive backwards at least 2 inches (I recommend 3 inches)
         runtime.reset();
-        
         robot.leftDrive.setPower(-1);
         robot.rightDrive.setPower(-1);
-
-        while(runtime.seconds()<2) {
+        while(opModeIsActive() && runtime.seconds()<1) {
+            telemetry.addData("Backing Up!", runtime);
+            telemetry.update();
         }
-
-        //lower scissor lift
-        runtime.reset();
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
 
         //rotate entire robot (clockwise from above) approx. 90 degrees (move left track forward and right track backward)
         runtime.reset();
-
-        while(runtime.seconds()<3) {
-            robot.leftDrive.setPower(1);
-            robot.rightDrive.setPower(-1);
+        double rotation = 3.66666; // time it takes robot to rotate ones
+        robot.leftDrive.setPower(1);
+        robot.rightDrive.setPower(-1);
+        while(opModeIsActive() && runtime.seconds()< rotation / 4) {
+            telemetry.addData("Rotating!", runtime);
+            telemetry.update();
         }
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
 
         //drive backwards into crater (about 10 seconds of movement is good)
         runtime.reset();
-
-        while(runtime.seconds()<10) {
-            robot.leftDrive.setPower(-1);
-            robot.rightDrive.setPower(-1);
+        robot.leftDrive.setPower(-1);
+        robot.rightDrive.setPower(-1);
+        while(opModeIsActive() && runtime.seconds()< 5) {
+            telemetry.addData("Backing Up!", runtime);
+            telemetry.update();
         }
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
     }
 }
 
