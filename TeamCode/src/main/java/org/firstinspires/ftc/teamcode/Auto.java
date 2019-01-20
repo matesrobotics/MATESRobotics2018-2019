759d23cccbf1c7d3134d09980490d3d2ac0cef10
Brady <school.bnichols@gmail.com>
2019-01-20 15:49:58 -0500

    Updated Auto.java as of 1/17/2018

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
@@ -9,6 +14,13 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Auto extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     RobotMap robot = new RobotMap();
+    double lidPos = 1;
+
+    //@Override //Code to run ONCE when the driver hits INIT
+    //public void init() {
+    //  robot.init(hardwareMap);
+
+    //}
 
     @Override
     public void runOpMode() {
@@ -18,8 +30,8 @@ public class Auto extends LinearOpMode {
         robot.init(hardwareMap);
         runtime.reset();
         robot.lift.setPower(1);
-        while(opModeIsActive() && runtime.seconds()<13) {
-            telemetry.addData("Raising!", runtime);
+        while(opModeIsActive() && runtime.seconds()<15.5) {
+            telemetry.addData("Lowering to Ground!", runtime);
             telemetry.update();
         }
         robot.lift.setPower(0);
@@ -37,14 +49,15 @@ public class Auto extends LinearOpMode {
 
         runtime.reset();
         while(opModeIsActive() && runtime.seconds()< 1) {
+            
         }
 
         //rotate entire robot (clockwise from above) approx. 90 degrees (move left track forward and right track backward)
         runtime.reset();
-        double rotation = 3.66666; // time it takes robot to rotate ones
-        robot.leftDrive.setPower(-1);
-        robot.rightDrive.setPower(1);
-        while(opModeIsActive() && runtime.seconds()< rotation) {
+        double rotation = 3.66666; // time it takes robot to rotate 360 degrees
+        robot.leftDrive.setPower(1);
+        robot.rightDrive.setPower(-1);
+        while(opModeIsActive() && runtime.seconds()< .9) {
             telemetry.addData("Rotating!", runtime);
             telemetry.update();
         }
@@ -52,28 +65,69 @@ public class Auto extends LinearOpMode {
         robot.rightDrive.setPower(0);
 
         //drive backwards into crater (about 10 seconds of movement is good)
-        //Note: it's only 4 seconds for now for easier testing
+        //Note: it's only 1 second for now for easier testing
         runtime.reset();
-        robot.leftDrive.setPower(-1);
-        robot.rightDrive.setPower(-1);
-        while(opModeIsActive() && runtime.seconds()< 4) {
-            telemetry.addData("Backing Up!", runtime);
+        robot.leftDrive.setPower(1);
+        robot.rightDrive.setPower(1);
+        while(opModeIsActive() && runtime.seconds()< 2.8) {
+            telemetry.addData("Going Forward!", runtime);
             telemetry.update();
         }
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
-
+        
+        //Using arm to get Around
+        runtime.reset();
+        robot.arm.setPower(.5);
+        while(opModeIsActive() && runtime.seconds()<1) {
+            telemetry.addData("Arm Active!", runtime);
+            telemetry.update();
+        }
+        robot.arm.setPower(0);
+        robot.arm.setPower(0);
+        
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds()<0.5) {
+            telemetry.addData("Waiting!", runtime);
+            telemetry.update();
+        }
+        
+        
         //Go down at the end
         runtime.reset();
         robot.lift.setPower(-1);
-        while(opModeIsActive() && runtime.seconds()<12) {
-            telemetry.addData("Lowering!", runtime);
+        while(opModeIsActive() && runtime.seconds()<5) {
+            telemetry.addData("Lowering 1!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
+        
+       lidPos = 0;
+        
+        robot.lid.setPosition(lidPos);
+        
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds()<1) {
+            telemetry.addData("Lowering 2!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
+        lidPos = 1;
+        
+        robot.lid.setPosition(lidPos);
+ 
+        while(opModeIsActive() && runtime.seconds()<11) {
+            telemetry.addData("Lowering 3!", runtime);
             telemetry.update();
             if (robot.touch.getState()) {
                 break;
             }
         }
         robot.lift.setPower(0);
-    }
+}
 }
 
