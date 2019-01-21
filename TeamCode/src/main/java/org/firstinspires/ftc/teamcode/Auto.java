134903c2e6a2573859e9a07a1493fb54bab8ff4f
Brady <school.bnichols@gmail.com>
2019-01-20 21:25:03 -0500

    Updated comments
    I just added comments or made other comments more descriptive so it was easier to figure out what everything did.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -16,19 +16,19 @@ public class Auto extends LinearOpMode {
     RobotMap robot = new RobotMap();
     double lidPos = 1;
 
-    //@Override //Code to run ONCE when the driver hits INIT
+    //I have no idea why these comments below are here but I'm leaving them anyway
+    //@Override
+    //Code to run ONCE when the driver hits INIT
     //public void init() {
     //  robot.init(hardwareMap);
-
     //}
-
-    //Just testing if I set Android Studio up correctly don't mind me ~Brady
 
     @Override
     public void runOpMode() {
 
         waitForStart();
 
+        //Lowers robot to ground from hook
         robot.init(hardwareMap);
         runtime.reset();
         robot.lift.setPower(1);
@@ -38,7 +38,7 @@ public class Auto extends LinearOpMode {
         }
         robot.lift.setPower(0);
 
-        //drive backwards at least 2 inches (I recommend 3 inches)
+        //Backs robot away from hook
         runtime.reset();
         robot.leftDrive.setPower(-1);
         robot.rightDrive.setPower(-1);
@@ -49,25 +49,25 @@ public class Auto extends LinearOpMode {
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
 
+        //Waits after backing up
         runtime.reset();
         while(opModeIsActive() && runtime.seconds()< 1) {
             
         }
 
-        //rotate entire robot (clockwise from above) approx. 90 degrees (move left track forward and right track backward)
+        //Rotates robot (clockwise from above) approx. 90 degrees (move left track forward and right track backward)
         runtime.reset();
-        double rotation = 3.66666; // time it takes robot to rotate 360 degrees
+        double rotation = 3.66666; // Approx. time it takes robot to rotate 360 degrees
         robot.leftDrive.setPower(1);
         robot.rightDrive.setPower(-1);
-        while(opModeIsActive() && runtime.seconds()< .9) {
+        while(opModeIsActive() && runtime.seconds()< .9) { //This is a decimal so it's easier to modify slightly
             telemetry.addData("Rotating!", runtime);
             telemetry.update();
         }
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
 
-        //drive backwards into crater (about 10 seconds of movement is good)
-        //Note: it's only 1 second for now for easier testing
+        //Drives to front of depot
         runtime.reset();
         robot.leftDrive.setPower(1);
         robot.rightDrive.setPower(1);
@@ -78,7 +78,7 @@ public class Auto extends LinearOpMode {
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
         
-        //Using arm to get Around
+        //Swings arm in front of robot
         runtime.reset();
         robot.arm.setPower(.5);
         while(opModeIsActive() && runtime.seconds()<1) {
@@ -87,7 +87,8 @@ public class Auto extends LinearOpMode {
         }
         robot.arm.setPower(0);
         robot.arm.setPower(0);
-        
+
+        //Waits after moving arm
         runtime.reset();
         while(opModeIsActive() && runtime.seconds()<0.5) {
             telemetry.addData("Waiting!", runtime);
@@ -95,7 +96,8 @@ public class Auto extends LinearOpMode {
         }
         
         
-        //Go down at the end
+        //Lowers before releasing team marker
+        //NOTE: The robot.touch.getState() section each time the bot lowers is the Currie switch
         runtime.reset();
         robot.lift.setPower(-1);
         while(opModeIsActive() && runtime.seconds()<5) {
@@ -105,11 +107,12 @@ public class Auto extends LinearOpMode {
                 break;
             }
         }
-        
-       lidPos = 0;
-        
+
+        //Opens lid
+        lidPos = 0;
         robot.lid.setPosition(lidPos);
-        
+
+        //Lowers slightly more while team marker is falling out
         runtime.reset();
         while(opModeIsActive() && runtime.seconds()<1) {
             telemetry.addData("Lowering 2!", runtime);
@@ -118,10 +121,12 @@ public class Auto extends LinearOpMode {
                 break;
             }
         }
+
+        //Closes lid
         lidPos = 1;
-        
         robot.lid.setPosition(lidPos);
- 
+
+        //Lowers until end of autonomous period
         while(opModeIsActive() && runtime.seconds()<11) {
             telemetry.addData("Lowering 3!", runtime);
             telemetry.update();
