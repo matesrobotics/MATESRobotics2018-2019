3aa7686d373c3dd1c6d7b2b5f695210032478485
Brady <school.bnichols@gmail.com>
2019-01-27 12:19:01 -0500

    Create ExperimentalAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExperimentalAuto.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class Auto extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    RobotMap robot = new RobotMap();
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+        robot.init(hardwareMap);
+
+    }
