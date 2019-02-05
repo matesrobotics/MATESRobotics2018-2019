582f1c10a2338607d3c3647c4324a7b0b934eb59
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2019-02-05 14:55:23 -0500

    Update NoDeploy.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoDeploy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoDeploy.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
 
-public class AutoPit extends LinearOpMode {
+public class NoDeploy extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     RobotMap robot = new RobotMap();
     double lidPos = 1;
@@ -28,69 +28,73 @@ public class AutoPit extends LinearOpMode {
         waitForStart();
 
         robot.init(hardwareMap);
+        
         runtime.reset();
         robot.lift.setPower(1);
-        while(opModeIsActive() && runtime.seconds()<15.5) {
-            telemetry.addData("Lowering to Ground!", runtime);
+        while(opModeIsActive() && runtime.seconds()<6) {
+            telemetry.addData("Raising for Arm!", runtime);
             telemetry.update();
         }
         robot.lift.setPower(0);
+        
+        // runtime.reset();
+        // robot.lift.setPower(1);
+        // while(opModeIsActive() && runtime.seconds()<15.5) {
+        //     telemetry.addData("Lowering to Ground!", runtime);
+        //     telemetry.update();
+        // }
+        // robot.lift.setPower(0);
 
-        //drive backwards at least 2 inches (I recommend 3 inches)
-        runtime.reset();
-        robot.leftDrive.setPower(-1);
-        robot.rightDrive.setPower(-1);
-        robot.lift.setPower(-1);
-        while(opModeIsActive() && runtime.seconds()<0.215) {
-            telemetry.addData("Backing Up!", runtime);
-            telemetry.update();
-            if (robot.touch.getState()) {
-                break;
-            }
+        // //drive backwards at least 2 inches (I recommend 3 inches)
+        // runtime.reset();
+        // robot.leftDrive.setPower(-1);
+        // robot.rightDrive.setPower(-1);
+        // robot.lift.setPower(-1);
+        // while(opModeIsActive() && runtime.seconds()<0.215) {
+        //     telemetry.addData("Backing Up!", runtime);
+        //     telemetry.update();
+        //     if (robot.touch.getState()) {
+        //         break;
+        //     }
+            
+        // }
+        // robot.leftDrive.setPower(0);
+        // robot.rightDrive.setPower(0);
 
-        }
-        robot.leftDrive.setPower(0);
-        robot.rightDrive.setPower(0);
-
-        runtime.reset();
-        while(opModeIsActive() && runtime.seconds()< 1) {
-
-        }
+        // runtime.reset();
+        // while(opModeIsActive() && runtime.seconds()< 1) {
+            
+        // }
 
         //rotate entire robot (clockwise from above) approx. 90 degrees (move left track forward and right track backward)
-        runtime.reset();
-        double rotation = 3.66666; // time it takes robot to rotate 360 degrees
-        robot.leftDrive.setPower(1);
-        robot.rightDrive.setPower(-1);
-        while(opModeIsActive() && runtime.seconds()< .9) {
-            telemetry.addData("Rotating!", runtime);
-            telemetry.update();
-            if (robot.touch.getState()) {
-                break;
-            }
-        }
-        robot.leftDrive.setPower(0);
-        robot.rightDrive.setPower(0);
+        // runtime.reset();
+        // double rotation = 3.66666; // time it takes robot to rotate 360 degrees
+        // robot.leftDrive.setPower(1);
+        // robot.rightDrive.setPower(-1);
+        // while(opModeIsActive() && runtime.seconds()< .9) {
+        //     telemetry.addData("Rotating!", runtime);
+        //     telemetry.update();
+        //     if (robot.touch.getState()) {
+        //         break;
+        //     }
+        // }
+        // robot.leftDrive.setPower(0);
+        // robot.rightDrive.setPower(0);
 
-        //drive backwards into crater (about 10 seconds of movement is good)
-        //Note: it's only 1 second for now for easier testing
+        //drive forwards into depot
         runtime.reset();
-        robot.lift.setPower(-1);
         robot.leftDrive.setPower(1);
         robot.rightDrive.setPower(1);
-        while(opModeIsActive() && runtime.seconds()< 2.8) {
+        while(opModeIsActive() && runtime.seconds()< 2.3) {
             telemetry.addData("Going Forward!", runtime);
             telemetry.update();
-            if (robot.touch.getState()) {
-                break;
-            }
         }
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
-
+        
         //Using arm to get Around
         runtime.reset();
-        robot.arm.setPower(.5);
+        robot.arm.setPower(.3);
         while(opModeIsActive() && runtime.seconds()<1) {
             telemetry.addData("Arm Active!", runtime);
             telemetry.update();
@@ -100,7 +104,7 @@ public class AutoPit extends LinearOpMode {
         }
         robot.arm.setPower(0);
         robot.arm.setPower(0);
-
+        
         runtime.reset();
         while(opModeIsActive() && runtime.seconds()<0.5) {
             telemetry.addData("Waiting!", runtime);
@@ -109,16 +113,56 @@ public class AutoPit extends LinearOpMode {
                 break;
             }
         }
-
-
-        //Go down at the end
-        runtime.reset();
-
+        
+        
+        
+    
         lidPos = 0;
-
-        robot.lid.setPosition(lidPos);
-
-        while(opModeIsActive() && runtime.seconds()<5) {
+        robot.lid.setPosition(lidPos); //opens
+        
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds()<1) {
+            telemetry.addData("Waiting!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
+        
+        runtime.reset();
+        robot.arm.setPower(.2);
+        while(opModeIsActive() && runtime.seconds()<.05) {
+            telemetry.addData("Jerk!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
+        
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds()<1) {
+            telemetry.addData("Waiting!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
+        
+        lidPos = 1;
+        robot.lid.setPosition(lidPos); //closes
+        
+        runtime.reset();
+        robot.arm.setPower(-.3);
+        while(opModeIsActive() && runtime.seconds()<1.2) {
+            telemetry.addData("Arm Active!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
+        
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds()<2) { //lowers before moving (4)
             robot.lift.setPower(-1);
             telemetry.addData("Lowering 1!", runtime);
             telemetry.update();
@@ -126,42 +170,33 @@ public class AutoPit extends LinearOpMode {
                 break;
             }
         }
-
-
-        runtime.reset();
-        while(opModeIsActive() && runtime.seconds()<1) {
-            telemetry.addData("Lowering 2!", runtime);
-            telemetry.update();
-            if (robot.touch.getState()) {
-                break;
-            }
-        }
         robot.lift.setPower(0);
-        lidPos = 1;
-
-        robot.lid.setPosition(lidPos);
-
+        
+       
+        
         runtime.reset();
+        
 
-        robot.leftDrive.setPower(-1);
-        robot.rightDrive.setPower(1);
-        while(opModeIsActive() && runtime.seconds()< 1.2) {
+        while(opModeIsActive() && runtime.seconds()< 1.4) {
+            robot.leftDrive.setPower(1);
+            robot.rightDrive.setPower(-1);
             telemetry.addData("Rotating!", runtime);
             telemetry.update();
         }
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
-
+        
+        //originally 3 
         runtime.reset();
         robot.leftDrive.setPower(1);
         robot.rightDrive.setPower(1);
-        while(opModeIsActive() && runtime.seconds()< 3) {
+        while(opModeIsActive() && runtime.seconds()< 4) {
             telemetry.addData("Going Forward!", runtime);
             telemetry.update();
         }
         robot.leftDrive.setPower(0);
         robot.rightDrive.setPower(0);
-
+ 
         // while(opModeIsActive() && runtime.seconds()<11) {
         //     telemetry.addData("Lowering 3!", runtime);
         //     telemetry.update();
@@ -170,6 +205,15 @@ public class AutoPit extends LinearOpMode {
         //     }
         // }
         robot.lift.setPower(0);
-    }
+        
+        runtime.reset();
+        robot.arm.setPower(.3);
+        while(opModeIsActive() && runtime.seconds()<1) {
+            telemetry.addData("Arm Active!", runtime);
+            telemetry.update();
+            if (robot.touch.getState()) {
+                break;
+            }
+        }
 }
-
+}
